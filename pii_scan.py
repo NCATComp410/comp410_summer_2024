470faf15101fb3ebe65a7c1ebb4a248b4c3bf7d9
claesmk <claesmk@gmail.com>
2024-05-28 20:20:05 -0400

    Initial project setup

---
+++ pii_scan.py
@@ -0,0 +1,62 @@
+"""PII Scan"""
+import spacy
+from presidio_analyzer import AnalyzerEngine, RecognizerRegistry
+from presidio_analyzer.predefined_recognizers import (ItDriverLicenseRecognizer,
+                                                      ItVatCodeRecognizer,
+                                                      ItFiscalCodeRecognizer,
+                                                      ItIdentityCardRecognizer,
+                                                      ItPassportRecognizer,
+                                                      EsNifRecognizer,
+                                                      PlPeselRecognizer)
+from presidio_anonymizer import AnonymizerEngine
+
+# make sure en_core_web_lg is loaded correctly
+# this can also be achieved with
+# python -m spacy download en_core_web_lg
+try:
+    nlp = spacy.load("en_core_web_lg")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_lg")
+    nlp = spacy.load("en_core_web_lg")
+
+# Create an analyzer object
+registry = RecognizerRegistry()
+registry.load_predefined_recognizers()
+# Add some language specific recognizers as english instead of default language
+registry.add_recognizer(ItDriverLicenseRecognizer(supported_language='en'))
+registry.add_recognizer(ItVatCodeRecognizer(supported_language='en'))
+registry.add_recognizer(ItFiscalCodeRecognizer(supported_language='en'))
+registry.add_recognizer(ItIdentityCardRecognizer(supported_language='en'))
+registry.add_recognizer(ItPassportRecognizer(supported_language='en'))
+registry.add_recognizer(EsNifRecognizer(supported_language='en'))
+registry.add_recognizer(PlPeselRecognizer(supported_language='en'))
+analyzer = AnalyzerEngine(registry=registry)
+anonymizer = AnonymizerEngine()
+
+
+def show_aggie_pride():
+    """Show Aggie Pride"""
+    return "Aggie Pride - Worldwide"
+
+
+def anonymize_text(text: str, entity_list: list) -> str:
+    """
+    Anonymize the text using the entity list
+    :param text: the text to be anonymized
+    :param entity_list: the list of entities to be anonymized
+           https://microsoft.github.io/presidio/supported_entities/
+    """
+    # Call analyzer to get results
+    results = analyzer.analyze(text=text,
+                               entities=entity_list,
+                               language='en')
+
+    # Analyzer results are passed to the AnonymizerEngine for anonymization
+    anonymized_text = anonymizer.anonymize(text=text, analyzer_results=results)
+
+    return anonymized_text.text
+
+
+if __name__ == '__main__':
+    show_aggie_pride()
---
+++ test_pii_scan.py
@@ -0,0 +1,35 @@
+"""Basic tests for the pii_scan module"""
+import unittest
+from pii_scan import show_aggie_pride,  anonymize_text
+
+
+class TestPII(unittest.TestCase):
+    """Test the pii_scan module"""
+    def test_show_aggie_pride(self):
+        """Test to make sure Aggie Pride is shown correctly"""
+        self.assertEqual(show_aggie_pride(), "Aggie Pride - Worldwide")
+
+    def test_anonymize_location(self):
+        """Test to make sure location is anonymized"""
+
+        # The first positive test case expects the Presido library to anonymize the location
+        # with "New York" replaced with "<LOCATION>"
+        self.assertEqual('I live in <LOCATION>',
+                         anonymize_text('I live in New York', ['LOCATION']))
+
+        # This negative test case expects the Presido library to not anonymize the location
+        # so anonymize_text should return the original text without any replacements
+        self.assertEqual('I live in a house',
+                         anonymize_text('I live in a house', ['LOCATION']))
+
+    def test_language_specific(self):
+        """Test to make sure language specific entities are loaded correctly"""
+        entities = ('ES_NIF, IT_DRIVER_LICENSE, IT_VAT_CODE, IT_FISCAL_CODE, IT_IDENTITY_CARD, '
+                    'IT_PASSPORT, PL_PESEL, SG_NRIC_FIN')
+        for e in entities.split(', '):
+            # make sure an exception is not raised
+            anonymize_text('my name is John Doe', [e])
+
+
+if __name__ == '__main__':
+    unittest.main()
