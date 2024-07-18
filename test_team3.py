d937f420525d0519ed1f61b2061598f00610f052
Shymik Smith <ybebannk@gmail.com>
2024-07-18 00:18:13 +0000

    adding positive IP ADDRESS test case

--- test_team3.py
+++ test_team3.py
@@ -16,3 +16,25 @@ class TestTeam3(unittest.TestCase):
 
         self.assertEqual('My NRP is Unknown',
                          anonymize_text('My NRP is Unknown', ['NRP']))
+
+    def test_ip_address(self):
+        """"Test to make sure a IP Adress is recognized"""
+        #positive test case
+       # prefix = '192'
+       # mid = '.158.'
+       # ending = '38'
+       # test_ipv4 = prefix + mid + ending
+        test_ipv4 = '192.168.1.1'
+        test_string = 'My IPv4 Address is: ' + test_ipv4
+        expected_result = 'My IPv4 Address is: <IP_ADDRESS>'
+        actual_result = anonymize_text(test_string, ['IP_ADDRESS'])
+        self.assertEqual(expected_result,
+                            actual_result)
+        #negative test case
+        test_ipv4= '12345'
+        test_string = 'My IPv4 Address is: ' + test_ipv4
+        expected_result = 'My IPv4 Address is: 12345'
+        actual_result = anonymize_text(test_string, ['IP_ADDRESS'])
+        self.assertEqual(expected_result,
+                            actual_result)
+        
