2a9223db72ba9a066aee01060c924f76c2b4bd30
LailaRH20 <56034352+LailaRH20@users.noreply.github.com>
2024-07-19 21:11:13 +0000

    Add US_ITIN Case Final Commit

--- test_team_null.py
+++ test_team_null.py
@@ -14,39 +14,39 @@ class TestTeamNull(unittest.TestCase):
         self.assertEqual('My NRP is <NRP>',
                          anonymize_text('My NRP is Democrat', ['NRP']))
         
-    def test_us_itin(self):
+    def test_us_ssn(self):
         """Test to make sure a US_SSN is recognized"""
         # positive test case
         prefix = '123'
         mid = '-12-'
         ending = '1234'
-        test_itin = prefix + mid + ending
-        test_string = 'My ITIN is ' + test_itin
-        expected_result = 'My ITIN is <US_ITIN>'
-        actual_result = anonymize_text(test_string, ['US_ITIN'])
+        test_ssn = prefix + mid + ending
+        test_string = 'My SSN is ' + test_ssn
+        expected_result = 'My SSN is <US_SSN>'
+        actual_result = anonymize_text(test_string, ['US_SSN'])
         self.assertEqual(expected_result,
                          actual_result)
         
-        # negative test case - US_ITIN is not replaced
-        test_itin = '123456789'
-        test_string = 'My ITIN is ' + test_itin
-        expected_result = 'My ITIN is 123456789'
-        actual_result = anonymize_text(test_string, ['US_ITIN'])
+        # negative test case - US_SSN is not replaced
+        test_ssn = '123456789'
+        test_string = 'My SSN is ' + test_ssn
+        expected_result = 'My SSN is 123456789'
+        actual_result = anonymize_text(test_string, ['US_SSN'])
         self.assertEqual(expected_result,
                          actual_result)
         
         # negative test case - too short
-        test_itin = '12345678'
-        test_string = 'My ITIN is ' + test_itin
-        expected_result = 'My ITIN is 12345678'
-        actual_result = anonymize_text(test_string, ['US_ITIN'])
+        test_ssn = '12345678'
+        test_string = 'My SSN is ' + test_ssn
+        expected_result = 'My SSN is 12345678'
+        actual_result = anonymize_text(test_string, ['US_SSN'])
         self.assertEqual(expected_result,
                          actual_result)
         
         # negative test case - too long
-        test_itin = '1234567890'
-        test_string = 'My ITIN is ' + test_itin
-        expected_result = 'My ITIN is 1234567890'
-        actual_result = anonymize_text(test_string, ['US_ITIN'])
+        test_ssn = '1234567890'
+        test_string = 'My ssn is ' + test_ssn
+        expected_result = 'My ssn is 1234567890'
+        actual_result = anonymize_text(test_string, ['US_SSN'])
         self.assertEqual(expected_result,
                          actual_result)
