9e0816a5f67cf4648d786912e344be5f170e0dd3
JaKhari2 <159212934+JaKhari2@users.noreply.github.com>
2024-07-19 09:30:57 +0000

    Added DATE_TIME test case

--- test_team3.py
+++ test_team3.py
@@ -16,3 +16,32 @@ class TestTeam3(unittest.TestCase):
 
         self.assertEqual('My NRP is Unknown',
                          anonymize_text('My NRP is Unknown', ['NRP']))
+        
+    def test_date_time(self):
+            """Test to make sure a DATE_TIME is recognized"""
+            # positive test case 
+            prefix = '12'
+            mid ='-12-'
+            ending ='1234'
+            test_date_time = prefix + mid + ending
+            test_string = 'The date is ' + test_date_time
+            expected_result = 'The date is <DATE_TIME>'
+            actual_result = anonymize_text(test_string, ['DATE_TIME'])
+            self.assertEqual(expected_result,
+                                actual_result)
+            
+             # negative test case - DATE_TIME is not replaced
+            test_date_time ='12345678'
+            test_string = 'The date is ' + test_date_time
+            expected_result = 'The date is 12345678'
+            actual_result = anonymize_text(test_string, ['DATE_TIME'])
+            self.assertEqual(expected_result,
+                                actual_result)
+            
+            # negative test case - too short
+            test_date_time ='1234567'
+            test_string = 'The date is ' + test_date_time
+            expected_result = 'The date is 1234567'
+            actual_result = anonymize_text(test_string, ['DATE_TIME'])
+            self.assertEqual(expected_result,
+                                actual_result)
