904eed4a59b4adeb6c55ce571ca40434de968d27
Brenda Arellano <barellanor91@gmail.com>
2024-07-19 20:29:35 +0000

    Added US_DRIVER_LICENSE test case

--- test_traveling_sales.py
+++ test_traveling_sales.py
@@ -17,49 +17,35 @@ class TestTeamTravelingSalesmen(unittest.TestCase):
         self.assertEqual('My NRP is Unknown',
                          anonymize_text('My NRP is Unknown', ['NRP']))
         
-    def test_au_medicare(self):
-        """Test to make sure an AU_MEDICARE is recognized"""
-        # negative test case 
-        test_medicare = '3888 87568 0'
-        test_string = 'My medicare number is ' + test_medicare
-        expected_result = 'My medicare number is 3888 87568 0'
-        actual_result = anonymize_text(test_string, ['AU_MEDICARE'])
-        self.assertEqual(expected_result,
-                         actual_result)
+    def test_us_driver_license(self):
+        """Test to make sure a US_DRIVER_LICENSE is recognized"""
+        # positive test case
+        prefix = '11'
+        mid = '1234567'
+        ending = '1'
+        test_dls = prefix + mid + ending
+        test_string = 'My driver license is ' + test_dls
+        expected_result = 'My driver license is <US_DRIVER_LICENSE>'
+        actual_result = anonymize_text(test_string, ['US_DRIVER_LICENSE'])
+        self.assertEqual(expected_result, actual_result)
+
+        # negative test case - US_DRIVER_LICENSE not matching pattern
+        test_dls = '1253463511'
+        test_string = 'My driver license is ' + test_dls
+        expected_result = 'My driver license is <US_DRIVER_LICENSE>'  # Expect anonymized value
+        actual_result = anonymize_text(test_string, ['US_DRIVER_LICENSE'])
+        self.assertEqual(expected_result, actual_result)
         
-        # negative test case "Too short"
-        test_medicare = '3888 8768 0'
-        test_string = 'My medicare number is ' + test_medicare
-        expected_result = 'My medicare number is 3888 8768 0'
-        actual_result = anonymize_text(test_string, ['AU_MEDICARE'])
-        self.assertEqual(expected_result,
-                         actual_result)
+        # negative test case - too short
+        test_dls = '1253445'
+        test_string = 'My driver license is ' + test_dls
+        expected_result = 'My driver license is <US_DRIVER_LICENSE>'  # Expect anonymized value
+        actual_result = anonymize_text(test_string, ['US_DRIVER_LICENSE'])
+        self.assertEqual(expected_result, actual_result)
         
-        # negative test case "Too short"
-        test_medicare = '38884 87868 0'
-        test_string = 'My medicare number is ' + test_medicare
-        expected_result = 'My medicare number is 38884 87868 0'
-        actual_result = anonymize_text(test_string, ['AU_MEDICARE'])
-        self.assertEqual(expected_result,
-                         actual_result)
-        
-        #positive test case "medium"
-        start = '3111 '
-        middle = '32311 '
-        end = '1'
-        test_medicare = start + middle + end
-        test_string = 'My medicare number is ' + test_medicare
-        expected_result = 'My medicare number is <AU_MEDICARE>'
-        actual_result = anonymize_text(test_string, ['AU_MEDICARE'])
-        self.assertEqual(expected_result,
-                         actual_result)
-        
-        #positive test case "low"
-        first = '21113'
-        last = '22311'
-        test_medicare = first + last
-        test_string = 'My medicare number is ' + test_medicare
-        expected_result = 'My medicare number is <AU_MEDICARE>'
-        actual_result = anonymize_text(test_string, ['AU_MEDICARE'])
-        self.assertEqual(expected_result,
-                         actual_result)
+        # negative test case - too long
+        test_dls = '1253463511123'
+        test_string = 'My driver license is ' + test_dls
+        expected_result = 'My driver license is <US_DRIVER_LICENSE>'  # Expect anonymized value
+        actual_result = anonymize_text(test_string, ['US_DRIVER_LICENSE'])
+        self.assertEqual(expected_result, actual_result)
